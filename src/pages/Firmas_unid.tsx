d3c09127f5fff1f8706b4863355e21c9abfca467
LeonardoTelis <jdelossantostelis@gmail.com>
2022-09-19 09:39:53 -0500

    first commit

---
+++ src/pages/Firmas_unid.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavButtons } from '../components/NavButtons';
+import { IonButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+
+const Firmas_unid: React.FC = () => {
+    return (
+<IonPage>
+    <IonHeader>
+        <IonToolbar>
+            <IonTitle>Iniciar sesion</IonTitle>
+            <IonButtons slot="end">
+            <NavButtons/>
+            </IonButtons>
+        </IonToolbar>
+    </IonHeader>
+    <IonContent fullscreen>
+    </IonContent>
+</IonPage>
+    );
+};
+
+export default Firmas_unid;
